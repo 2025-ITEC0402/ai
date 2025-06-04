ad05efa62f189d58f2778caa52fbc5232ec1c8f7
Songmin Lee <songmin3329@gmail.com>
2025-06-04 22:58:32 +0900

    feat: 배포용 도커 설정

--- main/main.py
+++ main/main.py
@@ -366,4 +366,4 @@ async def answer_query(payload: QAImageRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="debug")
+    uvicorn.run("main:app",host="0.0.0.0", port=8000, reload=False, log_level="info")
